ddbeae73779be52f4646f588a7d2fcbbf7d2f2f6
escc1122 <escc1122@gamil.com>
2021-11-21 17:04:48 +0800

    test channel

---
+++ channel/README.md
@@ -0,0 +1,5 @@
+參考
+
+https://blog.kennycoder.io/2020/12/23/Golang%E6%95%99%E5%AD%B8%E7%B3%BB%E5%88%97-%E4%BD%95%E8%AC%82Channel-%E5%85%88%E5%BE%9E%E5%AE%A3%E5%91%8AChannel%E9%96%8B%E5%A7%8B%E5%AD%B8%E8%B5%B7/
+
+https://peterhpchen.github.io/2020/03/08/goroutine-and-channel.html
---
+++ channel/hello_channel.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//練習基本的channel
+func hello() {
+	var stringChen = make(chan string, 3)
+	stringChen <- "hello"
+	stringChen <- "channel"
+	fmt.Println(<-stringChen)
+	fmt.Println(<-stringChen)
+}
+
+// channel & Goroutine
+func hello2() {
+	var stringChen = make(chan string, 3)
+	go func() {
+		time.Sleep(1000 * time.Millisecond)
+		stringChen <- "hello"
+	}()
+
+	go func() {
+		time.Sleep(5000 * time.Millisecond)
+		stringChen <- "world"
+	}()
+
+	fmt.Println(<-stringChen)
+	fmt.Println(<-stringChen)
+}
+
+//channel & range
+func withRange() {
+	c := make(chan int, 10)
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		close(c) // 關閉 Channel
+	}()
+	for i := range c { // 在 close 後跳出迴圈
+		fmt.Println(i)
+	}
+}
+
+//channel & select
+func withSelect() {
+	ch := make(chan string)
+	go func() {
+		fmt.Println("calculate goroutine starts calculating")
+		time.Sleep(time.Second) // Heavy calculation
+		fmt.Println("calculate goroutine ends calculating")
+
+		ch <- "FINISH"
+		time.Sleep(time.Second)
+		fmt.Println("calculate goroutine finished")
+	}()
+
+	for {
+		select {
+		case <-ch: // Channel 中有資料執行此區域
+			fmt.Println("main goroutine finished")
+			return
+		default: // Channel 阻塞的話執行此區域
+			fmt.Println("WAITING...")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+func main() {
+	// hello()
+	// hello2()
+	// withRange()
+	withSelect()
+}
