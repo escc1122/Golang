c80fd5aecacef916023c7accf3edaa2895624421
escc1122 <escc1122@gamil.com>
2021-11-25 10:27:09 +0800

    test 爬蟲

---
+++ test2/main.go
@@ -0,0 +1,65 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 241.
+
+// Crawl2 crawls web links starting with the command-line arguments.
+//
+// This version uses a buffered channel as a counting semaphore
+// to limit the number of concurrent calls to links.Extract.
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"test2/links"
+)
+
+//!+sema
+// tokens is a counting semaphore used to
+// enforce a limit of 20 concurrent requests.
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{} // acquire a token
+	list, err := links.Extract(url)
+	<-tokens // release the token
+
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+//!-sema
+
+//!+
+func main() {
+	worklist := make(chan []string)
+	var n int // number of pending sends to worklist
+
+	// Start with the command-line arguments.
+	urls := []string{"https://golang.org", "https://go.dev/solutions/paypal"}
+
+	n++
+	go func() { worklist <- urls }()
+
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		list := <-worklist
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				n++
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+}
+
+//!-
