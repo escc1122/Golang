90741e80872c56ad32ba5393872b8b79d8999355
al_ma <al_ma@paradise-soft.com.tw>
2023-05-11 17:27:21 +0800

    add

--- reflect_test/getAddr_test.go
+++ reflect_test/getAddr_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -21,5 +22,38 @@ func TestAddress_getAddr(t *testing.T) {
 	fmt.Println(address.getAddr2(address1))
 	fmt.Println(address.getAddr2(address2))
 	fmt.Println(address.getAddr2(address3))
+}
 
+func TestAddress_getAddr2(t *testing.T) {
+	address := &Address{}
+	testMap := make(map[string]string)
+	fmt.Println("reflect.TypeOf(testMap).Kind(): ", reflect.TypeOf(testMap).Kind())
+	testMap2 := &testMap
+	fmt.Println("reflect.TypeOf(testMap).Kind(): ", reflect.TypeOf(testMap2).Kind())
+
+	func(inTestMap map[string]string) {
+		fmt.Println("in: ", address.getAddr(testMap))
+	}(testMap)
+
+	fmt.Println("out: ", address.getAddr(testMap))
+}
+
+// 淺層複製
+func TestAddress_getAddr3(t *testing.T) {
+	//address := Address{}
+	p1 := Person{Name: "John", Age: 30}
+	p2 := p1 // 複製 p1 到 p2
+
+	p2.Name = "test"
+
+	fmt.Println(&p1) // &{John 30}
+	fmt.Println(&p2) // &{test 30}
+
+	p3 := &Person{Name: "John", Age: 30}
+	p4 := p3
+
+	p4.Name = "test5"
+
+	fmt.Println(p3) // &{test5 30}
+	fmt.Println(p4) // &{test5 30}
 }
